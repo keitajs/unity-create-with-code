60d872093d2b215e5c71b842b592f3cb30532194
keitajs <tejfolos.anime@gmail.com>
2023-09-22 14:02:46 +0200

    asd

---
+++ Unity Projects/Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float time = 4;
+    private float spawnTime = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (time >= spawnTime && Input.GetKeyDown(KeyCode.Space))
+        {
+            time = 0;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+
+        time += Time.deltaTime;
+    }
+}
