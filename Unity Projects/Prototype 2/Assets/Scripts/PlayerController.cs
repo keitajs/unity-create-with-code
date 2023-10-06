22ddb449dee1b02a721b566563f8df134746a778
keitajs <tejfolos.anime@gmail.com>
2023-10-06 15:49:16 +0200

    ✔

--- Unity Projects/Prototype 2/Assets/Scripts/PlayerController.cs
+++ Unity Projects/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 20.0f;
-    public float xRange = 10;
+    public float xRange = 20.0f;
     public GameObject projectilePrefab;
 
     // Start is called before the first frame update
