0f19a81a7318d1f446d1788aa10bf0a4c248f938
keitajs <tejfolos.anime@gmail.com>
2023-10-20 12:19:23 +0200

    -

--- Unity Projects/Prototype 4/Assets/Scripts/SpawnManager.cs
+++ Unity Projects/Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -5,18 +5,28 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
+    public int enemyCount;
+    public int waveNumber = 1;
     private float spawnRange = 9;
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, GeneratePosition(), enemyPrefab.transform.rotation);
+        SpawnPowerup();
+        SpawnEnemyWave(waveNumber);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if (enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnPowerup();
+            SpawnEnemyWave(waveNumber);
+        }
     }
 
     private Vector3 GeneratePosition()
@@ -25,5 +35,18 @@ public class SpawnManager : MonoBehaviour
         float spawnPosZ = Random.Range(-spawnRange, spawnRange);
         Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
         return randomPos;
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GeneratePosition(), enemyPrefab.transform.rotation);
+        }
+    }
+
+    void SpawnPowerup()
+    {
+        Instantiate(powerupPrefab, GeneratePosition(), powerupPrefab.transform.rotation);
     }
 }
